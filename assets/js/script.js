6e5061338c638dde58392c4eccbe2721e0ad6915
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2019-12-30 21:47:37 -0500

    Used js to deal with hiding and proper sections with button click events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,59 +1,136 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    let initialTime = 60;
     let time = 20;
-    let initialScore = 0;
     let score = 0;
     let qCount = 0;
-    
+    let recordsArray = [];
+   
     //Question Initial data
-    //console.log(questions);
-    //console.log(questions[0].choices[0]);
     document.querySelector('#quizHolder p').innerHTML = questions[0].title;
     document.querySelector('#quizHolder ol li:nth-of-type(1) div').innerHTML = `1. ${questions[qCount].choices[0]}`;
     document.querySelector('#quizHolder ol li:nth-of-type(2) div').innerHTML = `2. ${questions[qCount].choices[1]}`;
     document.querySelector('#quizHolder ol li:nth-of-type(3) div').innerHTML = `3. ${questions[qCount].choices[2]}`;
     document.querySelector('#quizHolder ol li:nth-of-type(4) div').innerHTML = `4. ${questions[qCount].choices[3]}`;
-    
+   
+    // Handles time related events
     let myTimer = () => {
-      if(time > 0 ) {
+     if (time > 0) {
       time = time - 1;
       document.getElementById('time').innerHTML = time;
-    } else{
+     } else {
       clearInterval(clock);
       document.getElementById('score').innerHTML = score;
       document.getElementById('finish').classList.remove('hide');
       document.getElementById('quizHolder').classList.add('hide');
+     }
     }
-    }
-    
+   
+    // Handles what happens when time runs out
     let clock;
-      document.querySelector("#intro button").addEventListener("click", (e) =>{
-          document.getElementById('intro').classList.add('hide');
-          document.getElementById('quizHolder').classList.remove('hide');
-          clock = setInterval(myTimer, 1000);
+    document.querySelector("#intro button").addEventListener("click", (e) => {
+     document.getElementById('intro').classList.add('hide');
+     document.getElementById('quizHolder').classList.remove('hide');
+     clock = setInterval(myTimer, 1000);
     });
-    
+   
+    // Create an array of selected divs so I can refer to them with the this keyword and replace their values to then check against the answer property for all questions.
     let answers = document.querySelectorAll('#quizHolder ol li div');
-    
     Array.from(answers).forEach(check => {
-      check.addEventListener('click', function(event) {
-        if( this.innerHTML.substring(3,this.length) === questions[qCount].answer ){
-        console.log(questions[qCount].answer);
-        console.log(this.innerHTML.substring(3,this.length));
-        }
+     check.addEventListener('click', function(event) {
+      if (this.innerHTML.substring(3, this.length) === questions[qCount].answer) {
+       score = score + 1;
+       qCount = qCount + 1;
+       if (qCount === questions.length) {
+        document.getElementById('quizHolder').classList.add('hide');
+        document.getElementById('finish').classList.remove('hide');
+        time = 0;
+        document.getElementById('time').innerHTML = time;
+       } else {
+        document.querySelector('#quizHolder p').innerHTML = questions[qCount].title;
+        document.querySelector('#quizHolder ol li:nth-of-type(1) div').innerHTML = `1. ${questions[qCount].choices[0]}`;
+        document.querySelector('#quizHolder ol li:nth-of-type(2) div').innerHTML = `2. ${questions[qCount].choices[1]}`;
+        document.querySelector('#quizHolder ol li:nth-of-type(3) div').innerHTML = `3. ${questions[qCount].choices[2]}`;
+        document.querySelector('#quizHolder ol li:nth-of-type(4) div').innerHTML = `4. ${questions[qCount].choices[3]}`;
+       }
+      } else {
+       time = time - 10;
+       if (time < 0) {
+        time = 0;
+        document.getElementById('time').innerHTML = time;
+       }
+       qCount = qCount + 1;
+       if (qCount === questions.length) {
+        document.getElementById('quizHolder').classList.add('hide');
+        document.getElementById('finish').classList.remove('hide');
+       } else {
+        document.querySelector('#quizHolder p').innerHTML = questions[qCount].title;
+        document.querySelector('#quizHolder ol li:nth-of-type(1) div').innerHTML = `1. ${questions[qCount].choices[0]}`;
+        document.querySelector('#quizHolder ol li:nth-of-type(2) div').innerHTML = `2. ${questions[qCount].choices[1]}`;
+        document.querySelector('#quizHolder ol li:nth-of-type(3) div').innerHTML = `3. ${questions[qCount].choices[2]}`;
+        document.querySelector('#quizHolder ol li:nth-of-type(4) div').innerHTML = `4. ${questions[qCount].choices[3]}`;
+       }
+      }
+     });
+    });
+   
+   
+    document.querySelector("#records button").addEventListener("click", () => {
+     let initialsRecord = document.querySelector('#initials').value
+     recordsArray.push(`${initialsRecord}  ${score}`);
+     document.querySelector('#highScores ol').innerHTML = '';
+     document.getElementById('finish').classList.add('hide');
+     document.getElementById('highScores').classList.remove('hide');
+   
+     Array.from(recordsArray).forEach(check => {
+      var score = document.createElement("li");
+      score.innerHTML = check;
+      document.querySelector('#highScores ol').appendChild(score);
+     });
+     document.querySelector("#initials").value = '';
+    });
+   
+   
+     document.querySelector("#clearScores").addEventListener("click", () => {
+      recordsArray = [];
+      document.querySelector('#highScores ol').innerHTML = "";
+     }); 
+   
+     
+     document.querySelector("#reset").addEventListener("click", () => {
+      time = 20;
+      score = 0;
+      qCount = 0;
+      document.querySelector('#quizHolder p').innerHTML = questions[qCount].title;
+      document.querySelector('#quizHolder ol li:nth-of-type(1) div').innerHTML = `1. ${questions[qCount].choices[0]}`;
+      document.querySelector('#quizHolder ol li:nth-of-type(2) div').innerHTML = `2. ${questions[qCount].choices[1]}`;
+      document.querySelector('#quizHolder ol li:nth-of-type(3) div').innerHTML = `3. ${questions[qCount].choices[2]}`;
+      document.querySelector('#quizHolder ol li:nth-of-type(4) div').innerHTML = `4. ${questions[qCount].choices[3]}`;
+      let sections = document.querySelectorAll("section");
+      Array.from(sections).forEach((userItem) => {
+        userItem.classList.add('hide');
       });
+      document.getElementById('intro').classList.remove('hide');
+     }); 
+   
+  
+   
+   
+    document.getElementById("scores").addEventListener("click", (e) => {
+     e.preventDefault();
+     time = 20;
+     score = 0;
+     qCount = 0;
+     document.querySelector('#quizHolder p').innerHTML = questions[qCount].title;
+     document.querySelector('#quizHolder ol li:nth-of-type(1) div').innerHTML = `1. ${questions[qCount].choices[0]}`;
+     document.querySelector('#quizHolder ol li:nth-of-type(2) div').innerHTML = `2. ${questions[qCount].choices[1]}`;
+     document.querySelector('#quizHolder ol li:nth-of-type(3) div').innerHTML = `3. ${questions[qCount].choices[2]}`;
+     document.querySelector('#quizHolder ol li:nth-of-type(4) div').innerHTML = `4. ${questions[qCount].choices[3]}`;
+     let sections = document.querySelectorAll("section");
+      Array.from(sections).forEach((userItem) => {
+        userItem.classList.add('hide');
+      });
+     document.getElementById('highScores').classList.remove('hide');
     });
-    
-    
-    /*document.querySelectorAll('#quizHolder ol li:nth-of-type(1) div').addEventListener("click", (e) =>{
-      console.log(e.target.innerHTML);
-    });*/
-    
-    
-    
-    
-    
-    
-    
-    });
+   
+   
+   });
