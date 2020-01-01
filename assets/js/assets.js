9a1d00dc58f012ab2f4afea1fe8aa178cbbb2b46
Steven Ferreira <57821462+steven-f206@users.noreply.github.com>
2019-12-31 22:04:55 -0500

    Added comments to explain code

--- assets/js/assets.js
+++ assets/js/assets.js
@@ -9,5 +9,20 @@ var questions = [
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
+    {
+      title: "Which of these is not used to loop?",
+      choices: ["for", "while", "foreach", "sequence"],
+      answer: "sequence"
+    },
+    {
+      title: "Which of these is not a way to save a variable",
+      choices: ["vet", "var", "let", "const"],
+      answer: "vet"
+    },
+    {
+      title: "JS date function starts in seconds to current day from what day in 1970",
+      choices: ["January 1", "December 31", "June 1", "April 23"],
+      answer: "January 1"
+    },
     ///etc.
   ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    let time = 20;
+    const initialTime = 75
+    let time = 75;
     let score = 0;
     let qCount = 0;
     let recordsArray = [];
    
     //Set the question Initial data in questionHolder section
-    document.querySelector('#quizHolder p').innerHTML = questions[0].title;
-    document.querySelector('#quizHolder ol li:nth-of-type(1) div').innerHTML = `1. ${questions[qCount].choices[0]}`;
-    document.querySelector('#quizHolder ol li:nth-of-type(2) div').innerHTML = `2. ${questions[qCount].choices[1]}`;
-    document.querySelector('#quizHolder ol li:nth-of-type(3) div').innerHTML = `3. ${questions[qCount].choices[2]}`;
-    document.querySelector('#quizHolder ol li:nth-of-type(4) div').innerHTML = `4. ${questions[qCount].choices[3]}`;
+    let qtitle = document.querySelector('#quizHolder p')
+    let q1 = document.querySelector('#quizHolder ol li:nth-of-type(1) div');
+    let q2 = document.querySelector('#quizHolder ol li:nth-of-type(2) div');
+    let q3 = document.querySelector('#quizHolder ol li:nth-of-type(3) div');
+    let q4 = document.querySelector('#quizHolder ol li:nth-of-type(4) div');
+   
+    qtitle.innerHTML = questions[0].title;
+    q1.innerHTML = `1. ${questions[qCount].choices[0]}`;
+    q2.innerHTML = `2. ${questions[qCount].choices[1]}`;
+    q3.innerHTML = `3. ${questions[qCount].choices[2]}`;
+    q4.innerHTML = `4. ${questions[qCount].choices[3]}`;
    
     // Handles time related events
     let myTimer = () => {
@@ -25,43 +32,49 @@ document.addEventListener('DOMContentLoaded', (event) => {
      }
     }
    
-    // Handles what happens when time runs out
+    // On intro button click start time and giving questions
     let clock;
     document.querySelector("#intro button").addEventListener("click", (e) => {
      document.getElementById('intro').classList.add('hide');
      document.getElementById('quizHolder').classList.remove('hide');
      clock = setInterval(myTimer, 1000);
     });
-  
-    // Clears timeout if next question is answered before current timeout is reached
+   
+    // Clears timeout if next question is answered before current timeout is reached or if form element has a requirement not met.
     let timeset;
     let scoreIndicator = () => {
-      clearTimeout(timeset);
-      timeset = setTimeout(() => { document.querySelector('#scoreIndicator').classList.add('hide') }, 3000);
+     clearTimeout(timeset);
+     timeset = setTimeout(() => {
+      document.querySelector('#scoreIndicator').classList.add('hide')
+     }, 3000);
     }
    
     // Create an array of selected divs so I can refer to them with the this keyword and replace their values to then check against the answer property for all questions.
     let answers = document.querySelectorAll('#quizHolder ol li div');
     Array.from(answers).forEach(check => {
      check.addEventListener('click', function(event) {
+      // Handles events if a question is answered correctly
       if (this.innerHTML.substring(3, this.length) === questions[qCount].answer) {
        score = score + 1;
        qCount = qCount + 1;
        document.querySelector('#scoreIndicator p').innerHTML = "Correct!"
        document.querySelector('#scoreIndicator').classList.remove('hide', scoreIndicator());
+       // If all the questions have been answered exist the
        if (qCount === questions.length) {
         document.getElementById('quizHolder').classList.add('hide');
         document.getElementById('finish').classList.remove('hide');
         time = 0;
         document.getElementById('time').innerHTML = time;
        } else {
-        document.querySelector('#quizHolder p').innerHTML = questions[qCount].title;
-        document.querySelector('#quizHolder ol li:nth-of-type(1) div').innerHTML = `1. ${questions[qCount].choices[0]}`;
-        document.querySelector('#quizHolder ol li:nth-of-type(2) div').innerHTML = `2. ${questions[qCount].choices[1]}`;
-        document.querySelector('#quizHolder ol li:nth-of-type(3) div').innerHTML = `3. ${questions[qCount].choices[2]}`;
-        document.querySelector('#quizHolder ol li:nth-of-type(4) div').innerHTML = `4. ${questions[qCount].choices[3]}`;
+        // Updates copy in questions with the net array's question text.
+        qtitle.innerHTML = questions[qCount].title;
+        q1.innerHTML = `1. ${questions[qCount].choices[0]}`;
+        q2.innerHTML = `2. ${questions[qCount].choices[1]}`;
+        q3.innerHTML = `3. ${questions[qCount].choices[2]}`;
+        q4.innerHTML = `4. ${questions[qCount].choices[3]}`;
        }
       } else {
+        // Handles events if a question is answered incorrectly.
        time = time - 10;
        document.querySelector('#scoreIndicator p').innerHTML = "Wrong!";
        document.querySelector('#scoreIndicator').classList.remove('hide', scoreIndicator());
@@ -76,11 +89,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
         time = 0;
         document.getElementById('time').innerHTML = time;
        } else {
-        document.querySelector('#quizHolder p').innerHTML = questions[qCount].title;
-        document.querySelector('#quizHolder ol li:nth-of-type(1) div').innerHTML = `1. ${questions[qCount].choices[0]}`;
-        document.querySelector('#quizHolder ol li:nth-of-type(2) div').innerHTML = `2. ${questions[qCount].choices[1]}`;
-        document.querySelector('#quizHolder ol li:nth-of-type(3) div').innerHTML = `3. ${questions[qCount].choices[2]}`;
-        document.querySelector('#quizHolder ol li:nth-of-type(4) div').innerHTML = `4. ${questions[qCount].choices[3]}`;
+        qtitle.innerHTML = questions[qCount].title;
+        q1.innerHTML = `1. ${questions[qCount].choices[0]}`;
+        q2.innerHTML = `2. ${questions[qCount].choices[1]}`;
+        q3.innerHTML = `3. ${questions[qCount].choices[2]}`;
+        q4.innerHTML = `4. ${questions[qCount].choices[3]}`;
        }
       }
      });
@@ -88,66 +101,83 @@ document.addEventListener('DOMContentLoaded', (event) => {
    
    
     document.querySelector("#records button").addEventListener("click", () => {
+   
      let initialsRecord = document.querySelector('#initials').value;
-     recordsArray.push({"initialRecord":initialsRecord, "score":score});
-     console.log(recordsArray);
-     document.querySelector('#highScores div').innerHTML = '';
-     document.getElementById('finish').classList.add('hide');
-     document.getElementById('highScores').classList.remove('hide');
-  
-     var i = 1;
-     recordsArray.sort((a, b) => b.score - a.score);
-     Array.from(recordsArray).forEach(check => {
-      var scores = document.createElement("div");
-      scores.innerHTML = i + ". " + check.initialRecord  + " - " + check.score;
-      document.querySelector('#highScores div').appendChild(scores);
-      i = i + 1
-     });
-     i = 0;
-     
-     document.querySelector("#initials").value = '';
+     if (initialsRecord === '') {
+      document.querySelector('#scoreIndicator p').innerHTML = "You need at least 1 character";
+      document.querySelector('#scoreIndicator').classList.remove('hide', scoreIndicator());
+     } else if (initialsRecord.match(/[[A-Za-z]/) === null) {
+      document.querySelector('#scoreIndicator p').innerHTML = "Only letters for initials allowed.";
+      document.querySelector('#scoreIndicator').classList.remove('hide', scoreIndicator());
+     } else if (initialsRecord.length > 5) {
+      document.querySelector('#scoreIndicator p').innerHTML = "Maximum of 5 characters allowed.";
+      document.querySelector('#scoreIndicator').classList.remove('hide', scoreIndicator());
+     } else {
+      recordsArray.push({
+       "initialRecord": initialsRecord,
+       "score": score
+      });
+      console.log(recordsArray);
+      document.querySelector('#highScores div').innerHTML = '';
+      document.getElementById('finish').classList.add('hide');
+      document.getElementById('highScores').classList.remove('hide');
+   
+      var i = 1;
+      recordsArray.sort((a, b) => b.score - a.score);
+      Array.from(recordsArray).forEach(check => {
+       var scores = document.createElement("div");
+       scores.innerHTML = i + ". " + check.initialRecord + " - " + check.score;
+       document.querySelector('#highScores div').appendChild(scores);
+       i = i + 1
+      });
+      i = 0;
+   
+      document.querySelector("#initials").value = '';
+     }
     });
    
    
-     document.querySelector("#clearScores").addEventListener("click", () => {
-      recordsArray = [];
-      document.querySelector('#highScores div').innerHTML = "";
-     }); 
+    document.querySelector("#clearScores").addEventListener("click", () => {
+     recordsArray = [];
+     document.querySelector('#highScores div').innerHTML = "";
+    });
    
-     
-     document.querySelector("#reset").addEventListener("click", () => {
-      time = 20;
-      score = 0;
-      qCount = 0;
-      document.querySelector('#quizHolder p').innerHTML = questions[qCount].title;
-      document.querySelector('#quizHolder ol li:nth-of-type(1) div').innerHTML = `1. ${questions[qCount].choices[0]}`;
-      document.querySelector('#quizHolder ol li:nth-of-type(2) div').innerHTML = `2. ${questions[qCount].choices[1]}`;
-      document.querySelector('#quizHolder ol li:nth-of-type(3) div').innerHTML = `3. ${questions[qCount].choices[2]}`;
-      document.querySelector('#quizHolder ol li:nth-of-type(4) div').innerHTML = `4. ${questions[qCount].choices[3]}`;
-      let sections = document.querySelectorAll("section");
-      Array.from(sections).forEach((userItem) => {
-        userItem.classList.add('hide');
-      });
-      document.getElementById('intro').classList.remove('hide');
-     }); 
    
-  
+    document.querySelector("#reset").addEventListener("click", () => {
+     time = initialTime;
+     score = 0;
+     qCount = 0;
+     qtitle.innerHTML = questions[qCount].title;
+     q1.innerHTML = `1. ${questions[qCount].choices[0]}`;
+     q2.innerHTML = `2. ${questions[qCount].choices[1]}`;
+     q3.innerHTML = `3. ${questions[qCount].choices[2]}`;
+     q4.innerHTML = `4. ${questions[qCount].choices[3]}`;
+     let sections = document.querySelectorAll("section");
+     Array.from(sections).forEach((userItem) => {
+      userItem.classList.add('hide');
+     });
+     document.getElementById('intro').classList.remove('hide');
+    });
+   
+   
    
    
     document.getElementById("scores").addEventListener("click", (e) => {
      e.preventDefault();
-     time = 20;
+     clearInterval(clock);
+     document.getElementById('time').innerHTML = 0;
+     time = initialTime;
      score = 0;
      qCount = 0;
-     document.querySelector('#quizHolder p').innerHTML = questions[qCount].title;
-     document.querySelector('#quizHolder ol li:nth-of-type(1) div').innerHTML = `1. ${questions[qCount].choices[0]}`;
-     document.querySelector('#quizHolder ol li:nth-of-type(2) div').innerHTML = `2. ${questions[qCount].choices[1]}`;
-     document.querySelector('#quizHolder ol li:nth-of-type(3) div').innerHTML = `3. ${questions[qCount].choices[2]}`;
-     document.querySelector('#quizHolder ol li:nth-of-type(4) div').innerHTML = `4. ${questions[qCount].choices[3]}`;
+     qtitle.innerHTML = questions[qCount].title;
+     q1.innerHTML = `1. ${questions[qCount].choices[0]}`;
+     q2.innerHTML = `2. ${questions[qCount].choices[1]}`;
+     q3.innerHTML = `3. ${questions[qCount].choices[2]}`;
+     q4.innerHTML = `4. ${questions[qCount].choices[3]}`;
      let sections = document.querySelectorAll("section");
-      Array.from(sections).forEach((userItem) => {
-        userItem.classList.add('hide');
-      });
+     Array.from(sections).forEach((userItem) => {
+      userItem.classList.add('hide');
+     });
      document.getElementById('highScores').classList.remove('hide');
     });
    
